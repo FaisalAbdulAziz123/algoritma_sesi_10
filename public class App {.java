fca06d0e451a475df1c3efec6445ffcd91095ae8
FaisalAbdulAziz123 <114976859+FaisalAbdulAziz123@users.noreply.github.com>
2023-05-12 11:09:16 +0700

    Add files via upload

---
+++ public class App {.java
@@ -0,0 +1,73 @@
+public class App {
+    public static void main(String[] args) thorow Exception {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.println("Array setelah diurutkan:");
+        printArray(arr);
+
+        int[] arr2 = {64, 34, 25, 12, 22, 11, 90};
+        insertionSort(arr2);
+        System.out.println("Array 2 setelah diurutkan");
+        printArray(arr2);
+
+        
+        int[] arr3 = {64, 34, 25, 12, 22, 11, 90};
+        selectionSort(arr3);
+        System.out.println("Array 3 setelah diurutkan");
+        printArray(arr3);
+    }
+
+    public static void insertionSort(int[] arr){
+        int n = arr length;
+        for (int i = 1 < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr [j] > key) {
+                arr[j+ 1] = arr [j];
+                j = j - 1;
+        }
+        arr[j + 1] = key;
+     }
+  }
+
+  public static void selectionSort(int arr[])
+  {
+        int pos;
+        int temp;
+        for (int i = 0; i < arr.length; j++)
+        {
+            pos = i;
+            for (int j = i+1; j < arr.length; j++)
+            {
+                if (arr[j] < arr[pos]) 
+                {
+                    pos = j;
+                }
+            }
+            temp = arr[pos];
+            arr[pos] = arr[i];
+            arr[i] = temp;
+        }
+  }
+
+  public static void bubbleSort(int[] arr) {
+    int n = arr.length;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Tukar elemen arr[j] dengan arr[j = 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j1] = temp;
+            }
+        }
+    }
+  }
+
+  public static void printArray(int[] arr){
+    int n = arr.length;
+    for (int i = 0; i < n; i++) {
+        system.out.print(arr[i] + " ");
+    }
+    System.out.prinlin(;)
+  }
